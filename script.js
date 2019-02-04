d35f66bb51ce832a7b5ef3e9d1ca32160abe6aa2
Rachel Greider <you@example.com>
2019-02-04 14:16:33 -0500

    about page responsive

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log('hello world');
 
-var currentIndex = 0;
-var carousel;
-var carouselWannabes;
-window.onload = function () {
-    addCarouselElements();
-};
+// var currentIndex = 0;
+// var carousel;
+// var carouselWannabes;
+// window.onload = function () {
+//     addCarouselElements();
+// };
 
 // Get the modal
 // function runs when called by onclick html tag attached to all images
@@ -20,12 +20,13 @@ function openModal(modalID, imgID, imgNum) {
     //var modalImg = document.getElementById("img01");
 
     // use imgNum to set the current index of the carousel
-    currentIndex = imgNum-1;
+    //currentIndex = imgNum-1;
     //flip to that index
-    incrementCarousel(`carousel1`, 2);
+    //incrementCarousel(`carousel1`, 2);
 
     modal.style.display = "block";
     //modalImg.src = img.src;
+    modalImg.src = getElementById(`${imgID}`);
 
 
 
@@ -39,52 +40,52 @@ function openModal(modalID, imgID, imgNum) {
     }
 }
 
-function addCarouselElements() {
-    console.log("addCarouselElements()");
-    // add all image ID's to carousel array
+// function addCarouselElements() {
+//     console.log("addCarouselElements()");
+//     // add all image ID's to carousel array
 
-    carousel = document.getElementById("carousel1");
-    console.log(`carousel var?  ` + carousel);
-    carouselWannabes = document.getElementsByClassName("inCarousel");
-
-
-    // create number of divs equal to length of array
-    // append to parent carousel container
-    // assign carousel styling classes
-    var imageNum = carouselWannabes.length;
-    console.log(`Number of Images:  ${imageNum}`)
-    for (var i = 0; i < imageNum; i++) {
-        console.log(`carouselWannabes[i].innerHTML:   ` + carouselWannabes[i].innerHTML)
-        var newCarouselElement = `<div class="carouselElement">${carouselWannabes[i].innerHTML} </div>`;
-        console.log(`to add innerHTML:  ` + newCarouselElement);
-        carousel.innerHTML += newCarouselElement;
-    }
-}
+//     carousel = document.getElementById("carousel1");
+//     console.log(`carousel var?  ` + carousel);
+//     carouselWannabes = document.getElementsByClassName("inCarousel");
 
 
-// read in slider buttons
-// translate carousel 
-function incrementCarousel(carouselID, direction) {
-    console.log(`Incrementing Carousel (${carouselID})`);
+//     // create number of divs equal to length of array
+//     // append to parent carousel container
+//     // assign carousel styling classes
+//     var imageNum = carouselWannabes.length;
+//     console.log(`Number of Images:  ${imageNum}`)
+//     for (var i = 0; i < imageNum; i++) {
+//         console.log(`carouselWannabes[i].innerHTML:   ` + carouselWannabes[i].innerHTML)
+//         var newCarouselElement = `<div class="carouselElement">${carouselWannabes[i].innerHTML} </div>`;
+//         console.log(`to add innerHTML:  ` + newCarouselElement);
+//         carousel.innerHTML += newCarouselElement;
+//     }
+// }
 
-    var carousel = document.getElementById(`${carouselID}`);
-    var carouselButton = document.getElementsByClassName("slideButton");
 
-    console.log(`   carousel.style.left:  ` + carousel.style.left);
-    console.log(`   currentIndex:  ` + currentIndex);
-    carouselWannabes = document.getElementsByClassName("carouselElement");
+// // read in slider buttons
+// // translate carousel 
+// function incrementCarousel(carouselID, direction) {
+//     console.log(`Incrementing Carousel (${carouselID})`);
 
-    // translate 
-    //if trying to move right and is not equal to the number of images
-    if (direction === 1 && currentIndex != carouselWannabes.length - 1) {
-        currentIndex += 1;
-        console.log(currentIndex * (-75));
-        carousel.style.left = `${currentIndex * (-75)}vw`;
-    } else if (direction === 0 && currentIndex != 0) {
-        currentIndex -= 1;
-        console.log(currentIndex * (-75));
-        carousel.style.left = `${currentIndex * (-75)}vw`;
-    } else if (direction === 2) {
-        carousel.style.left = `${currentIndex * (-75)}vw`;
-    } else console.log("    failed");
-}
+//     var carousel = document.getElementById(`${carouselID}`);
+//     var carouselButton = document.getElementsByClassName("slideButton");
+
+//     console.log(`   carousel.style.left:  ` + carousel.style.left);
+//     console.log(`   currentIndex:  ` + currentIndex);
+//     carouselWannabes = document.getElementsByClassName("carouselElement");
+
+//     // translate 
+//     //if trying to move right and is not equal to the number of images
+//     if (direction === 1 && currentIndex != carouselWannabes.length - 1) {
+//         currentIndex += 1;
+//         console.log(currentIndex * (-75));
+//         carousel.style.left = `${currentIndex * (-75)}vw`;
+//     } else if (direction === 0 && currentIndex != 0) {
+//         currentIndex -= 1;
+//         console.log(currentIndex * (-75));
+//         carousel.style.left = `${currentIndex * (-75)}vw`;
+//     } else if (direction === 2) {
+//         carousel.style.left = `${currentIndex * (-75)}vw`;
+//     } else console.log("    failed");
+// }
