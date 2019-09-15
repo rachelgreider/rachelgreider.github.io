b3f906342d55801bcdef6b7444f00f293a6173c8
Rachel Greider <you@example.com>
2019-09-15 12:30:17 -0400

    js

--- script.js
+++ script.js
@@ -128,8 +128,11 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+ 
+  if(slides){
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
+ }
+ } 
 
